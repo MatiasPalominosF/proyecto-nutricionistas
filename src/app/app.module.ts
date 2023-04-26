aff1bafd470ac7827c9e3e9db6d2007b3df28ea8
Matías <matiiias.-15@live.cl>
2023-04-26 17:32:30 -0400

    Se instalan las dependencias de firebase
    Firebase
    Firebase tools 
    Angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -15,6 +20,9 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     SharedModule,
     HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    AngularFireAuthModule,
     BrowserAnimationsModule,
     AppRoutingModule
   ],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCTiZqZDXgZEyp-dXgwPjWpgtWex6Ep0O4",
+    authDomain: "nutricionistas-proyecto.firebaseapp.com",
+    projectId: "nutricionistas-proyecto",
+    storageBucket: "nutricionistas-proyecto.appspot.com",
+    messagingSenderId: "380567487116",
+    appId: "1:380567487116:web:b396d52d7e46b26b09f1a9",
+    measurementId: "G-338CG2P1GK"
+  }
 };
 
 /*
