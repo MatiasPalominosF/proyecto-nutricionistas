cf11234bcd88cdeb1a00a333b7dcefe5c48c58a2
Matías <matiiias.-15@live.cl>
2023-04-26 19:46:33 -0400

    Fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,16 @@
+import { credentialsFirebase } from "credentials";
+
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCTiZqZDXgZEyp-dXgwPjWpgtWex6Ep0O4",
-    authDomain: "nutricionistas-proyecto.firebaseapp.com",
-    projectId: "nutricionistas-proyecto",
-    storageBucket: "nutricionistas-proyecto.appspot.com",
-    messagingSenderId: "380567487116",
-    appId: "1:380567487116:web:b396d52d7e46b26b09f1a9",
-    measurementId: "G-338CG2P1GK"
+    firebase: {
+      apiKey: credentialsFirebase.credentials.apiKey,
+      authDomain: credentialsFirebase.credentials.authDomain,
+      projectId: credentialsFirebase.credentials.projectId,
+      storageBucket: credentialsFirebase.credentials.storageBucket,
+      messagingSenderId: credentialsFirebase.credentials.messagingSenderId,
+      appId: credentialsFirebase.credentials.appId,
+      measurementId: credentialsFirebase.credentials.measurementId
+    }
   }
 };
