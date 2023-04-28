c9f82eab7a965cd512e86ad26cb5d4605147836e
Matías <matiiias.-15@live.cl>
2023-04-28 12:43:13 -0400

    Fix env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,14 +3,12 @@ import { credentialsFirebase } from "credentials";
 export const environment = {
   production: true,
   firebase: {
-    firebase: {
-      apiKey: credentialsFirebase.credentials.apiKey,
-      authDomain: credentialsFirebase.credentials.authDomain,
-      projectId: credentialsFirebase.credentials.projectId,
-      storageBucket: credentialsFirebase.credentials.storageBucket,
-      messagingSenderId: credentialsFirebase.credentials.messagingSenderId,
-      appId: credentialsFirebase.credentials.appId,
-      measurementId: credentialsFirebase.credentials.measurementId
-    }
+    apiKey: credentialsFirebase.credentials.apiKey,
+    authDomain: credentialsFirebase.credentials.authDomain,
+    projectId: credentialsFirebase.credentials.projectId,
+    storageBucket: credentialsFirebase.credentials.storageBucket,
+    messagingSenderId: credentialsFirebase.credentials.messagingSenderId,
+    appId: credentialsFirebase.credentials.appId,
+    measurementId: credentialsFirebase.credentials.measurementId
   }
 };
